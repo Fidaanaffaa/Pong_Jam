ca1fa5c05cc286dfe8844b167b5de172b6e20658
amirboxer <amirboxer95@gmail.com>
2022-12-06 12:58:35 +0200

    Update PlayerMovement.cs
    playerMovment

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,11 @@ using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Reference point, center of circle, should be the center of the arena (the position of the camera) 
+ // Reference point, center of circle, should be the center of the arena (the position of the camera) 
     public Transform rotationCenter;
+    private Vector3 _rotationPosition;
+
+    public Collider2D playerBounds;
     
     // The initial angle of the object, can be changed if needed
     public float initialAngle = 0;
@@ -23,8 +26,7 @@ public class PlayerMovement : MonoBehaviour
 
     private float _posX, _posY, _angle = 0;
     private const float AngleLimit = 2.0f * Mathf.PI;
-    private const float PlayerUpperLimit = AngleLimit/2;
-    private const float PlayerLowerLimit = AngleLimit*3/4;
+    private List<List<bool>> _keyDisabled = new List<List<bool>>() {new List<bool>(){ false, false},new List<bool>() { false, false } };
 
     public enum PlayerSide
     {
@@ -40,37 +42,40 @@ public class PlayerMovement : MonoBehaviour
     
     // Start is called before the first frame update
     void Start()
-    {   
+    {
+        _rotationPosition = rotationCenter.position;
         _orientation = _sideToOrientation[side];
-        _angle += -((float)_orientation - 1)/4 * AngleLimit; 
+        _angle += -((float)_orientation - 1) / 4 * AngleLimit;
         _posX = rotationRadius * _orientation;
         _posY = 0;
         transform.position = new Vector3(_posX, _posY, 0);
     }
     
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other == playerBounds)
+        {
+            _keyDisabled[(_orientation-1)/-2][(int)(Mathf.Sign(_posY)-1)/-2] = true;
+        }
+    }
     
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other == playerBounds)
+        {
+            _keyDisabled[(_orientation - 1) / -2][(int)(Mathf.Sign(_posY) - 1) / -2] = false;
+        }
+    }
     
-    // private void OnTriggerStay2D(Collider2D col)
-    // {
-    //     updateAngle(-1); // ???????????????????????????????????????????????????????????????????????
-    //     _posX = rotationCenter.position.x + Mathf.Cos(_angle) * rotationRadius;
-    //     _posY = rotationCenter.position.y + Mathf.Sin(_angle) * rotationRadius;
-    //     transform.position = new Vector3(_posX, _posY, 0);
-    //     transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * _angle + initialAngle);
-    //     Debug.Log("hit hapanned");
-    // }
-    
-    
-    
-    private void updateAngle(int direction)
+    private void UpdateAngle(int direction)
     {
         _angle = (_angle + Time.deltaTime * angularSpeed * direction * _orientation + AngleLimit) % AngleLimit;
     }
 
-    private void move()
+    private void Move()
     {
-        _posX = rotationCenter.position.x + Mathf.Cos(_angle) * rotationRadius;
-        _posY = rotationCenter.position.y + Mathf.Sin(_angle) * rotationRadius;
+        _posX = _rotationPosition[0] + Mathf.Cos(_angle) * rotationRadius;
+        _posY = _rotationPosition[1] + Mathf.Sin(_angle) * rotationRadius;
         transform.position = new Vector3(_posX, _posY, 0);
         transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * _angle + initialAngle);
     }
@@ -78,28 +83,29 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) & side == PlayerSide.Right)
+        
+        if (Input.GetKey(KeyCode.UpArrow) & side == PlayerSide.Right & !_keyDisabled[0][0])
         {
-            updateAngle(1);
-            move();
+            UpdateAngle(1);
+            Move();
         }
 
-        if (Input.GetKey(KeyCode.DownArrow)& side == PlayerSide.Right)  
+        if (Input.GetKey(KeyCode.DownArrow)& side == PlayerSide.Right & !_keyDisabled[0][1])  
         {  
-            updateAngle(-1);
-            move();
+            UpdateAngle(-1);
+            Move();
         }
         
-        if (Input.GetKey(KeyCode.W) & side == PlayerSide.Left)
+        if (Input.GetKey(KeyCode.W) & side == PlayerSide.Left & !_keyDisabled[1][0])
         {
-            updateAngle(1);
-            move();
+            UpdateAngle(1);
+            Move();
         } 
         
-        if (Input.GetKey(KeyCode.S)& side == PlayerSide.Left)  
+        if (Input.GetKey(KeyCode.S)& side == PlayerSide.Left & !_keyDisabled[1][1])  
         {  
-            updateAngle(-1);
-            move();
+            UpdateAngle(-1);
+            Move();
         }
     }
 }
