2b2154bb7e74a41f5a147ea5bb65e7910ccbca5f
amirboxer <amirboxer95@gmail.com>
2022-12-08 15:02:44 +0200

    goals

--- Assets/Scripts/GoalLocation.cs
+++ Assets/Scripts/GoalLocation.cs
@@ -8,6 +8,7 @@ using Random = UnityEngine.Random;
 public class GoalLocation : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GoalSide Side;
     private float _scaleForPadding;
     private float _safetyParameter = 1.5f;
     private float _paddingAngle;
@@ -17,17 +18,28 @@ public class GoalLocation : MonoBehaviour
     private float _finishAngle = Mathf.PI * 3 / 2;
     private float _fullCircle;
     private float _circle;
-    private float _insideArena = 0.9f;
-    private float _angleOfPosition;
+    private float _insideArena = 0.95f;
+    private float _angleOfPosition;   // TODO SET IN COMPOMEMTS
     void Start()
     {
         _scaleForPadding = transform.localScale[_scaleIndex] / 2 + _safetyParameter;
         _paddingAngle = _scaleForPadding / Radius;
+        if (Side == GoalSide.Right)
+        {
+            _startAngle *= 3;
+            _finishAngle = _finishAngle * 5 / 3;
+        }
         float angle = _startAngle + _paddingAngle;
         float xPos = Radius * _insideArena * Mathf.Cos(angle);
         float yPos = Radius * _insideArena * Mathf.Sin(angle);
         transform.position = new Vector3(xPos, yPos, 0);
         transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * angle);
+    }
+    
+    public enum GoalSide
+    {
+        Left,
+        Right
     }
 
     void DrawRandomPosition()
@@ -38,13 +50,15 @@ public class GoalLocation : MonoBehaviour
         transform.position = new Vector3(xPos, yPos, 0);
         transform.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * ang);
     }
+    
+    
 
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.GetComponent<Collider2D>().CompareTag("ball"))
         {
             DrawRandomPosition();
-        };
+        }
     }
 
     // Update is called once per frame
